7719fd758ca981ab11c29445d31f8dbeac78dfff
Paradox <1254550919@qq.com>
2022-05-26 10:51:48 +0800

    Revert "fix: simplify resp handle logic"
    This reverts commit 29e1bed13a11434e06d8abfd4fb3f5953bf7aa1a.

--- transport/xhttp/server.go
+++ transport/xhttp/server.go
@@ -3,23 +3,22 @@ package xhttp
 import (
 	"context"
 	"crypto/tls"
-	"github.com/go-kratos/kratos/v2/encoding/msgpack"
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/internal/endpoint"
 	"github.com/go-kratos/kratos/v2/internal/host"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/valyala/fasthttp"
+	"github.com/vmihailenco/msgpack/v5"
 	"net"
 	"net/url"
 	"time"
 )
 
 // SupportPackageIsVersion1 These constants should not be referenced from any other code.
-const (
-	SupportPackageIsVersion1 = true
-	MsgPackName              = msgpack.Name
-)
+const SupportPackageIsVersion1 = true
+
+var _ = msgpack.UnregisterExt
 
 // ServerOption is an HTTP server option.
 type ServerOption func(*Server)
